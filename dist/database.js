577bb4dbb1b8d69af3ddf0c83ea44b53713bd561
ctrl1758 <franco.more1758@gmail.com>
2023-02-15 11:23:40 -0300

    se agrega altaProducto

--- dist/database.js
+++ dist/database.js
@@ -7,7 +7,7 @@ exports.AppDataSource = new typeorm_1.DataSource({
     type: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: '1234',
+    password: 'Pentagono123',
     database: 'TiendaRopa',
     port: 5432,
     entities: [models_1.BECategoria, models_1.BEProducto, models_1.BEDetalle, models_1.BETalle, models_1.BEColor],
