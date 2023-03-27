e7402950d8b7b446ec893e4c95576bbf4bed6194
jonatha1992 <jonathancorrea291292@hotmail.com>
2023-03-26 22:47:14 -0300

    cambio de host

--- backend/database.ts
+++ backend/database.ts
@@ -2,14 +2,26 @@ import { DataSource } from 'typeorm';
 import { BECategoria, BEProducto, BEColor, BEStock, BEUsuario } from './models';
 
 
+// export const AppDataSource = new DataSource({
+//     type: 'postgres',
+//     host: 'localhost',
+//     username: 'postgres',
+//     password: '1234',
+//     database: 'TiendaRopa',
+//     port: 5432,
+//     entities: [BECategoria, BEProducto, BEStock, BEColor, BEUsuario],
+//     logging: true,
+//     synchronize: false,
+// });
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: 'tiendaropa.internal',
     username: 'postgres',
-    password: '1234',
+    password: 'ujxRNhmNI9DeXfk',
     database: 'TiendaRopa',
     port: 5432,
     entities: [BECategoria, BEProducto, BEStock, BEColor, BEUsuario],
     logging: true,
-    synchronize: false
+    synchronize: false,
 });
--- dist/database.js
+++ dist/database.js
@@ -3,14 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 const models_1 = require("./models");
+// export const AppDataSource = new DataSource({
+//     type: 'postgres',
+//     host: 'localhost',
+//     username: 'postgres',
+//     password: '1234',
+//     database: 'TiendaRopa',
+//     port: 5432,
+//     entities: [BECategoria, BEProducto, BEStock, BEColor, BEUsuario],
+//     logging: true,
+//     synchronize: false,
+// });
 exports.AppDataSource = new typeorm_1.DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: 'tiendaropa.internal',
     username: 'postgres',
-    password: '1234',
+    password: 'ujxRNhmNI9DeXfk',
     database: 'TiendaRopa',
     port: 5432,
     entities: [models_1.BECategoria, models_1.BEProducto, models_1.BEStock, models_1.BEColor, models_1.BEUsuario],
     logging: true,
-    synchronize: false
+    synchronize: false,
 });
