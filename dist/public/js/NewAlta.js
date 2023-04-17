53c3d64b7a3162a36cd5df652f11b61798a395fd
Jonathan c <77857658+jonatha1992@users.noreply.github.com>
2023-04-17 09:32:58 -0300

    funcion seleccionar

--- dist/public/js/NewAlta.js
+++ dist/public/js/NewAlta.js
@@ -49,7 +49,7 @@ window.addEventListener("load", IniciarAPP);
 BtnGrabar.addEventListener("click", () => Agregar());
 BtnBuscar.addEventListener("click", () => Buscar(producto.id));
 BtnEditar.addEventListener("click", () => Editar(producto.id));
-BtnEliminar.addEventListener("click",() => Eliminar(producto.id));
+BtnEliminar.addEventListener("click", () => Eliminar(producto.id));
 
 id.addEventListener("input", AgregarCard);
 titulo.addEventListener("input", AgregarCard);
@@ -156,9 +156,9 @@ function LimpiarHtml(div) { // funcion para eliminar todos el contenido de los n
   }
 }
 
- function scrollSuave() {
+function scrollSuave() {
   $('body, html').animate({
-    scrollTop: '0px'
+    scrollTop: '20px'
   }, 300);
 };
 
@@ -224,6 +224,9 @@ function MostrarProductos() {
     let tr = document.createElement("tr");
     tr.id = producto.id;
 
+    let tdId = document.createElement("td");
+    tdId.textContent = producto.id;
+
     let tdTitulo = document.createElement("td");
     tdTitulo.textContent = producto.titulo;
 
@@ -246,28 +249,26 @@ function MostrarProductos() {
     let tdBtn = document.createElement("td");
 
     let BtnSeleccionar = document.createElement("button");
-    BtnSeleccionar.onclick = () => {
-      Buscar(tr.id)
-      scrollSuave()
-    }
-   
+    BtnSeleccionar.onclick = () => Seleccionar(tr.id)
 
-    
-      // formulario.focus();
-    BtnSeleccionar.classList.add('btn', 'btn-primary', 'me-2' ,"bg-primary");
+
+    // formulario.focus();
+    BtnSeleccionar.classList.add('btn', 'btn-primary', 'me-2', "bg-primary");
     BtnSeleccionar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hand-index-thumb-fill"                             viewBox="0 0 16 16">
                                  <path  d="M8.5 1.75v2.716l.047-.002c.312-.012.742-.016 1.051.046.28.056.543.18.738.288.273.152.456.385.56.642l.132-.012c.312-.024.794-.038 1.158.108.37.148.689.487.88.716.075.09.141.175.195.248h.582a2 2 0 0 1 1.99 2.199l-.272 2.715a3.5 3.5 0 0 1-.444 1.389l-1.395 2.441A1.5 1.5 0 0 1 12.42 16H6.118a1.5 1.5 0 0 1-1.342-.83l-1.215-2.43L1.07 8.589a1.517 1.517 0 0 1 2.373-1.852L5 8.293V1.75a1.75 1.75 0 0 1 3.5 0z" />
                             </svg>`
 
     tdBtn.appendChild(BtnSeleccionar)
-    
+
     let BtnEliminar = document.createElement("button");
     BtnEliminar.onclick = () => Eliminar(id);
-    BtnEliminar.classList.add('btn', 'btn-danger','me-2','bg-danger');
+    BtnEliminar.classList.add('btn', 'btn-danger', 'me-2', 'bg-danger');
     BtnEliminar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">   <path   d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" /> </svg>`
-    
+
     tdBtn.appendChild(BtnEliminar)
 
+    tr.appendChild(tdId)
+    tr.appendChild(tdId);
     tr.appendChild(tdTitulo);
     tr.appendChild(tdDescripcion);
     tr.appendChild(tdPrecio);
@@ -279,7 +280,13 @@ function MostrarProductos() {
   })
 }
 
+async function Seleccionar(id) {
 
+  producto = productos.find(x => x.id == id);
+  EstadoEdicion()
+  CargarFormulario()
+  scrollSuave()
+}
 
 async function AgregarImagen(event) {
   {
@@ -336,6 +343,7 @@ function EstadoAgregarBuscar() {
   Ocultar(BtnEditar)
   Ocultar(BtnEliminar)
   Visualizar(BtnGrabar)
+  Visualizar(BtnBuscar)
 }
 
 function EstadoEdicion() {
@@ -344,6 +352,7 @@ function EstadoEdicion() {
   Visualizar(BtnEditar)
   Visualizar(BtnEliminar)
   Ocultar(BtnGrabar)
+  Ocultar(BtnBuscar)
 }
 
 
