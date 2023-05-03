5675bbd3acee63e82022d0c5fb975e951705e790
jonatha1992 <jonathancorrea291292@hotmail.com>
2023-05-02 22:59:06 -0300

    se colocarion varios campos al stock

--- backend/models/BEStock.ts
+++ backend/models/BEStock.ts
@@ -1,38 +1,69 @@
-import { Entity, Column, OneToOne, JoinColumn, PrimaryGeneratedColumn, BaseEntity } from 'typeorm'
-import { BEProducto } from '.';
+import {
+     Entity,
+     Column,
+     OneToOne,
+     JoinColumn,
+     PrimaryGeneratedColumn,
+     BaseEntity,
+} from "typeorm";
+import { BEProducto } from ".";
 
-
-
-@Entity({ name: 'stock' })
+@Entity({ name: "stock" })
 export class BEStock extends BaseEntity {
+     @PrimaryGeneratedColumn({ type: "int" })
+     public id: number;
 
-  @PrimaryGeneratedColumn({ type: "int" })
-  public id: number
+     @OneToOne(() => BEProducto, (producto) => producto.stock, {
+          nullable: true,
+          onDelete: "CASCADE",
+     })
+     @JoinColumn({ name: "productoId" })
+     public producto: BEProducto;
 
-  @OneToOne(() => BEProducto, (producto) => producto.stock, { nullable: true, onDelete: 'CASCADE' })
-  @JoinColumn({ name: "productoId" })
-  public producto: BEProducto
+     @Column({nullable:true})
+     public XXS: number
 
-  // @Column()
-  // public XXS: number
+     @Column({nullable:true})
+     public XS: number
 
-  // @Column()
-  // public XS: number
+     @Column({nullable:true})
+     public Uno: number;
 
-  @Column()
-  public S: number
+     @Column({nullable:true})
+     public Dos: number;
 
-  @Column()
-  public M: number
+     @Column({nullable:true})
+     public Tres: number;
 
-  @Column()
-  public L: number
+     @Column({nullable:true})
+     public Cuatro: number;
 
-  @Column()
-  public XL: number
+     @Column({nullable:true})
+     public OchentaCinco: number;
 
-  // @Column()
-  // public XXL: number
+     @Column({nullable:true})
+     public Noventa: number;
 
+     @Column({nullable:true})
+     public NoventaCinco: number;
+     
+     @Column({nullable:true})
+     public Cien: number;
+     
+     @Column({nullable:true})
+     public S: number;
+     @Column({nullable:true})
+     public M: number;
 
+     @Column({nullable:true})
+     public L: number;
+
+     @Column({nullable:true})
+     public XL: number;
+
+     @Column({nullable:true})
+     public XXL: number;
+
+     @Column({nullable:true})
+     public General: number;
 }
