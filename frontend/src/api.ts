422bd6d20ff1e502997c4a7f4af7bdd9e8743abf
jonathan-escritorio <=>
2025-08-09 15:05:52 -0300

    Remove unused .firebaserc file, add new Firebase configuration files, update index.html title, and modify API base URL. Also, add new assets and adjust styles in style.css while deleting the old Tailwind configuration.

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -5,7 +5,7 @@ import type { Role, RoleType, User, Color, Category, Size } from './types';
 import type { Product } from './types';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
+  baseURL: import.meta.env.BACKEND_URL || 'http://localhost:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
