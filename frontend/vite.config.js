ae7963c0645ece717ae5d25c342aae36c255adc1
jonathan-zen <jonicorrea1992@gmail.com>
2025-08-17 21:07:06 -0300

    Update TypeScript configuration for bundler compatibility and add Vite configuration files

---
+++ frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+// https://vite.dev/config/
+// Configuración base Vite; las variables que empiecen por VITE_ se exponen automáticamente
+export default defineConfig({
+    plugins: [vue()]
+});
